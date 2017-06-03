0b1dbe94c4b02f0e5a2b368705a1aae9b7e8feb4
Michael <mhill421@gmail.com>
2017-06-02 19:56:52 -0700

    decided it didnt make sense to have a directory as source for the movie yet.. once we can take in more than the media

--- plexformatter/Formatters/MovieFormatter.cs
+++ plexformatter/Formatters/MovieFormatter.cs
@@ -41,6 +41,16 @@ namespace PlexFormatter.Formatters
 
         public string Year { get; set; } = null;
 
+        public PlexMedia Movie
+        {
+            get => Media.FirstOrDefault();
+            set
+            {
+                Media.Clear();
+                Media.Add(value);
+            }
+        }
+
         public MovieFormatter(BackgroundWorker worker, string source, string movieTitle, bool deleteSourceFiles, string plexRootDirectory, string year = null)
             : this(source, movieTitle, deleteSourceFiles, plexRootDirectory, year)
         {
@@ -49,53 +59,53 @@ namespace PlexFormatter.Formatters
 
         public MovieFormatter(string source, string movieTitle, bool deleteSourceFiles, string plexRootDirectory, string year = null)
         {
-            if (!Directory.Exists(source) && !File.Exists(source))
-                throw new DirectoryNotFoundException($"Could not find source directory: {source}"); //TODO custom exception
+            if (/*!Directory.Exists(source) && */!File.Exists(source))
+                throw new FileNotFoundException($"Could not find source file: {source}"); //TODO custom exception
 
             _deleteSourceFiles = deleteSourceFiles;
             _plexRootDirectory = PlexRootDirectory;
             Year = year;
 
             //directory or file
-            if ((File.GetAttributes(source) & FileAttributes.Directory) == FileAttributes.Directory)
-            {
-                new DirectoryInfo(source)
-                .EnumerateFiles()
-                .Where(f => VideoExtensions.Contains(f.Extension.Replace(".", "")))
-                .ToList()
-                .ForEach(f => Media.Add(new PlexMedia(f, movieTitle)));
-            }
-            else
-            {
+            //if ((File.GetAttributes(source) & FileAttributes.Directory) == FileAttributes.Directory)
+            //{
+            //    new DirectoryInfo(source)
+            //    .EnumerateFiles()
+            //    .Where(f => VideoExtensions.Contains(f.Extension.Replace(".", "")))
+            //    .ToList()
+            //    .ForEach(f => Media.Add(new PlexMedia(f, movieTitle)));
+            //}
+            //else
+            //{
                 Media.Add(new PlexMedia(new FileInfo(source), movieTitle));
-            }
+            //}
         }
 
         public override PlexFormatterResult Validate()
         {
             var log = new List<string>();
-            foreach (var movie in Media)
-            {
+            //foreach (var movie in Media)
+            //{
                 if (!string.IsNullOrEmpty(Year))
                 {
-                    movie.Year = Year;
+                    Movie.Year = Year;
                 }
                 else
                 {
                     _worker.ReportProgress(0, "No year provided, searching filename...");
                     //TODO return something more informative in case the implementer wants to prevent choices of the correct year to the user.
-                    var matches = rgx_yearKey.Matches(movie.SourceFile.Name);
+                    var matches = rgx_yearKey.Matches(Movie.SourceFile.Name);
                     if (matches.Count == 0)
-                        log.Add($"'{movie.SourceFile.Name}' missing year identifier.");
+                        log.Add($"'{Movie.SourceFile.Name}' missing year identifier.");
                     else if (matches.Count > 1)
-                        log.Add($"Found multiple year identifiers in '{movie.SourceFile.Name}'.");
+                        log.Add($"Found multiple year identifiers in '{Movie.SourceFile.Name}'.");
                     else
                     {
-                        movie.Year = matches[0].Value;
-                        _worker.ReportProgress(0, $"Found '{movie.Year}'");
+                        Movie.Year = matches[0].Value;
+                        _worker.ReportProgress(0, $"Found '{Movie.Year}'");
                     }
                 }
-            }
+            //}
 
             var result = new PlexFormatterResult();
             if (log.Count > 0)
@@ -118,12 +128,12 @@ namespace PlexFormatter.Formatters
             }
 
             var result = new PlexFormatterResult();
-            foreach (var movie in Media)
-            {
-                if (string.IsNullOrEmpty(movie.DestinationPath))
+            //foreach (var movie in Media)
+            //{
+                if (string.IsNullOrEmpty(Movie.DestinationPath))
                 {
                     var removed_chars = new List<char>();
-                    var title = $"{movie.Title} ({movie.Year})";
+                    var title = $"{Movie.Title} ({Movie.Year})";
                     var folderName = title;
                     if (InvalidPathChars.IsMatch(folderName))
                     {
@@ -132,7 +142,7 @@ namespace PlexFormatter.Formatters
                             removed_chars.Add(matches[i].Value[0]);
                         folderName = InvalidPathChars.Replace(folderName, string.Empty);
                     }
-                    var fileName = title + movie.SourceFile.Extension;
+                    var fileName = title + Movie.SourceFile.Extension;
                     if (InvalidPathChars.IsMatch(fileName))
                     {
                         var matches = InvalidPathChars.Matches(fileName);
@@ -145,9 +155,9 @@ namespace PlexFormatter.Formatters
                         result.Log.Add($"Removed invalid chars {string.Join(" ", removed_chars.Distinct())} from {title}");
                     }
                     var dbg = Path.Combine(PlexRootDirectory, folderName, fileName);
-                    movie.DestinationPath = Path.Combine(PlexRootDirectory, folderName, fileName);
+                    Movie.DestinationPath = Path.Combine(PlexRootDirectory, folderName, fileName);
                 }
-            }
+            //}
             return result.Finalize(PlexFormatterResult.ResultStatus.Success); 
         }
 
@@ -161,12 +171,12 @@ namespace PlexFormatter.Formatters
             }
 
             var result = new PlexFormatterResult();
-            foreach (var movie in Media)
-            {
+            //foreach (var movie in Media)
+            //{
                 try
                 {
-                    _worker.ReportProgress(0, $"Creating directory for {movie.Title}");
-                    Directory.CreateDirectory(movie.DestinationPath.Substring(0, movie.DestinationPath.LastIndexOf('\\')));
+                    _worker.ReportProgress(0, $"Creating directory for {Movie.Title}");
+                    Directory.CreateDirectory(Movie.DestinationPath.Substring(0, Movie.DestinationPath.LastIndexOf('\\')));
                 }
                 catch (Exception ex)
                 {
@@ -175,8 +185,8 @@ namespace PlexFormatter.Formatters
 
                 try
                 {
-                    _worker.ReportProgress(0, $"Copying source file for {movie.Title}");
-                    var copier = new ProgressReportingFileCopier(movie.SourceFile.FullName, movie.DestinationPath);
+                    _worker.ReportProgress(0, $"Copying source file for {Movie.Title}");
+                    var copier = new ProgressReportingFileCopier(Movie.SourceFile.FullName, Movie.DestinationPath);
                     copier.OnUpdate += (i) => _worker.ReportProgress(0, new CopyUpdate(i, i == 0));
                     copier.OnComplete += () => _worker.ReportProgress(0, "Complete!");
                     copier.Copy();
@@ -190,15 +200,15 @@ namespace PlexFormatter.Formatters
                 {
                     try
                     {
-                        _worker.ReportProgress(0, $"Deleting source file for {movie.Title}");
-                        movie.SourceFile.Delete();
+                        _worker.ReportProgress(0, $"Deleting source file for {Movie.Title}");
+                        Movie.SourceFile.Delete();
                     }
                     catch (Exception ex)
                     {
                         _worker.ReportProgress(0, $"Unable to delete source file. The error was: {ex.Message}");
                     }
                 }
-            }
+            //}
             return result.Finalize(PlexFormatterResult.ResultStatus.Success);
         }
     }
